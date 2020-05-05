6afb4b2f2d6f1d2c8f344b6c014fccddae4eb8b0
ML <3066947855@qq.com>
2020-05-05 15:31:02 +0800

    完成了帖子修改功能

--- post/views.py
+++ post/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from post.models import Post
 # Create your views here.
 def create_post(request):
-    if request.method =='POST':
+    if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
         post = Post.objects.create(title=title, content=content)
@@ -12,7 +12,19 @@ def create_post(request):
 
 
 def edit_post(request):
-    return render(request, 'edit_post.html', {})
+    if request.method == 'POST':
+        post_id = int(request.POST.get('post_id'))
+        # title = request.POST.get('title')
+        # content = request.POST.get('content')
+        post = Post.objects.get(pk=post_id)
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('/post/read/?post_id=%s' % post.id)
+    else:
+        post_id = int(request.GET.get('post_id'))
+        post = Post.objects.get(pk=post_id)
+        return render(request, 'edit_post.html', {'post' : post})
 
 
 def read_post(request):
